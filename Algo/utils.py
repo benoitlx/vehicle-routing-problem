9a283e28cbab12d09a129a8b90e891331dc9bd04
Benoit <benoitleroux41@gmail.com>
2024-03-12 10:57:24 +0100

    new representation of cycles

--- Algo/utils.py
+++ Algo/utils.py
@@ -1,6 +1,9 @@
-from random import shuffle
+from random import shuffle, randint, uniform
 from import_data import *
 import matplotlib.pyplot as plt
+from math import floor
+
+type cycles = np.ndarray[float]
 
 def random_cycles(n: int, m: int) -> list[list[int]]:
     """
@@ -13,7 +16,6 @@ def random_cycles(n: int, m: int) -> list[list[int]]:
     """
     
     cycles = [[] for _ in range(m)]
-    cycles[0].append(0)
     custs = list(range(1, n+1))
     shuffle(custs)
 
@@ -25,13 +27,22 @@ def random_cycles(n: int, m: int) -> list[list[int]]:
     for i in range(n):
         cycles[j].append(custs[i])
         if i in sep:
-            cycles[j].append(0)
             j += 1
-            cycles[j].insert(0, 0)
-
-    cycles[m-1].append(0)
 
     return cycles
+
+def random_float_cycles(n: int, m: int) -> cycles:
+    return np.array([randint(0, m-1) + uniform(0, 1) for _ in range(n)])
+
+def float_cycles_to_indices(c: cycles, m: int) -> list[list[int]]:
+    cycles = [[] for _ in range(m)]
+    indices = np.argsort(c) # O(n log n)
+    for i in indices:
+        vehicle_id = floor(c[i])
+        cycles[vehicle_id].append(i)
+
+    return cycles
+
 
 def distance_squared(a: tuple[float], b: tuple[float]) -> float:
     return (a[0]-b[0])**2 + (a[1]-b[1])**2
@@ -52,8 +63,14 @@ def visualize_cycles(cycles: list[list[int]], lat: list[float], lon: list[float]
 
     plt.show()
 
+
 if __name__ == "__main__":
     lat, lon = coordinates("2_detail_table_customers.xls", "4_detail_table_depots.xls")
+    """
     n = len(lat) - 1
     cycles = random_cycles(n, 8)
     visualize_cycles(cycles, lat, lon)
+    """
+    c = random_float_cycles(10, 3)
+    print(c)
+    print(float_cycles_to_indices(c, 3))
