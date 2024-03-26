26c78ffe1c022932a89b6edd0d4474237628bfd7
Justin Bauer <justin.bauer@centrale.centralelille.fr>
2024-03-26 09:21:05 +0100

    Cleaned up branch

--- Algo/recuit.py
+++ Algo/recuit.py
@@ -1,46 +1,56 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import random
-import utils as ut
+
+from utils import *
 
 """Algo recuit simulé"""
 
-def recuit_simule(solution_initiale,t0 = 10,nbiter_cycle = 1e3,a = 0.99):
+def recuit(solution_initiale, coord_dict, t0 = 10,nbiter_cycle = 1e2,a = 0.99):
   # X = une solution (initiale) qui fonctionne
   # t0 = température initiale
   # nbiter_cycle = nbr de paliers
   # a = coefficient de refroidissement
   
   #Initialisation
-  best_solution=np.copy(solution_initiale)
-  nbiter=0
-  nouveaucycle=True
-  t=t0
+  best_solution = np.copy(solution_initiale)
+  best_cost = cost(best_solution, coord_dict)
+  nbiter = 0
+  nouveaucycle = True
+  t = t0
 
   #Itération tant qu'on a pas atteint l'équilibre du système
-  while nouveaucycle==True: 
-    nbiter=0
-    nouveaucycle=False
-    while nbiter<nbiter_cycle:
-      nbiter+=1
+  while nouveaucycle == True: 
+    nbiter = 0
+    nouveaucycle = False
+    
+    while nbiter < nbiter_cycle:
+      nbiter += 1
 
-      solution_voisine=np.copy(best_solution)
-      solution_voisine = ut.random_swap(solution_voisine)
+      solution_voisine = np.copy(best_solution)
+      solution_voisine = swap_elements_cycle(solution_voisine)
 
       #On compare le coût des deux solutions 
-      difference=ut.cout(solution_voisine)-ut.cout(best_solution) 
+      neighbor_cost = cost(solution_voisine, coord_dict)
+      difference = neighbor_cost - best_cost
 
       #Si la nouvelle solution est meilleure que la précédente on la garde
-      if difference<0:
+      if difference < 0:
         best_solution=solution_voisine
+        best_cost = neighbor_cost
         nouveaucycle=True
       
       #Si elle est moins bonne, on peut la garder avec une certaine probabilité 
       else:
-        probabilite=np.exp(-difference/t)
-        q=random.random()
-        if q<probabilite:
-          best_solution=solution_voisine
-          nouveaucycle=True
-    t=a*t
+        probabilite = np.exp(-difference / t)
+        q = random.random()
 
-  return
+        if q < probabilite:
+          best_solution = solution_voisine
+          best_cost = neighbor_cost
+          nouveaucycle=True
+
+    t *= a
+
+  return [best_cost, best_solution]
