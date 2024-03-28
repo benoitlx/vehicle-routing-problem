897ef8ef485738cc796cc3585b7a27b03287862c
EliseFrc <elise.francoise@live.fr>
2024-03-28 09:35:23 +0100

    Update recuit_simule.py

--- Algo/recuit_simule.py
+++ Algo/recuit_simule.py
@@ -9,7 +9,8 @@ from utils import *
 """Algo recuit simulé"""
 
 def recuit_simule(solution_initiale, coord_dict, t0 = 10,nbiter_cycle = 1e2,a = 0.99):
-  # X = une solution (initiale) qui fonctionne
+  # Solution (initiale) qui fonctionne
+  # coord_dict = dictionnaire contenant l'id la latitude et la longitude du dépôt et de tous les clients
   # t0 = température initiale
   # nbiter_cycle = nbr de paliers
   # a = coefficient de refroidissement
@@ -26,6 +27,7 @@ def recuit_simule(solution_initiale, coord_dict, t0 = 10,nbiter_cycle = 1e2,a = 
     nbiter = 0
     nouveaucycle = False
     
+    #Tant que le cycle en cours n'a pas atteint une solution satisfaisante
     while nbiter < nbiter_cycle:
       nbiter += 1
 
@@ -52,12 +54,16 @@ def recuit_simule(solution_initiale, coord_dict, t0 = 10,nbiter_cycle = 1e2,a = 
           best_cost = neighbor_cost
           nouveaucycle=True
 
+    #Le système refroidit 
     t *= a
 
   return [best_cost, best_solution]
 
+
+#Création de l'Agent Recuit 
 class AgentRecuit(Agent):
   
+  #Initialisation de l'agent
   def __init__(self, unique_id, model, solution_initiale, coord_dict, t0 = 10,nbiter_cycle = 1e2,a = 0.99):
     super.__init__(unique_id, model)
 
@@ -68,16 +74,22 @@ class AgentRecuit(Agent):
     self.nbiter_cycle = nbiter_cycle
     self.a = a
 
+  #Définition de la fonction de contact permetant à 2 algos d'échanger leurs solutions
   def contact(self):
+    # On regarde tous les voisins $
     for agent in self.model.schedule.agents:
+      #Si l'un d'entre eux à un meilleur coût que nous, on le garde et on prend sa solution
       if agent.cost<self.cost:
           self.cost=agent.cost
           self.solution=agent.solution
 
+  #Définition de la fonction step
   def step(self):
+    #On calcule la solution grâce à l'algo de recuit 
     variable=recuit_simule([self.cost,self.solution],self.dict,self.t_init,self.nbiter_cycle,self.a) #Variable = [cout, [routes]]
     self.cost=variable[0]
     self.solution=variable[1]
+    #On compare les réultats des différents algos
     self.contact()
 
 
