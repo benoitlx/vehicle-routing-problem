8c1d7fca104b7dc1d7968c89b3d49eaa55ad96fd
EliseFrc <elise.francoise@live.fr>
2024-04-09 08:55:27 +0200

    Update apprentissage_par_renforcement.py

--- Algo/apprentissage_par_renforcement.py
+++ Algo/apprentissage_par_renforcement.py
@@ -3,12 +3,12 @@ from Qlearning import *
 
 def randomAction(state):
     f = random.choice(intra_route_swap, inter_route_swap, intra_route_shift, inter_route_shift, two_intra_route_swap, two_intra_route_shift, elimine_petite_route, elimine_route)
-    return f
+    return f(state)
 
-def choose_an_action(state, type_function, epsilon, q_size):
+def choose_an_action(state, type_function, epsilon, Q):
     next_state = 0
     if type_function == 1:
-        next_state = greedy(state, epsilon, q_size)
+        next_state = greedy(state, epsilon, Q)
     else:
         if type_function == 2:
             next_state = randomAction(state)
