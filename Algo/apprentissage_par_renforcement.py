1dbacba4126a47fd69034c21157063ba4d2e3dad
aude-pinson <audeanne.p@gmail.com>
2024-04-09 08:44:34 +0200

    Update apprentissage_par_renforcement.py

--- Algo/apprentissage_par_renforcement.py
+++ Algo/apprentissage_par_renforcement.py
@@ -1,12 +1,15 @@
-from voisinnage.py import *
-from Qlearning.py import *
+from voisinnage import *
+from Qlearning import *
 
+def randomAction(state):
+    f = random.choice(intra_route_swap, inter_route_swap, intra_route_shift, inter_route_shift, two_intra_route_swap, two_intra_route_shift, elimine_petite_route, elimine_route)
 
-def choose_an_action(state, type_function):
+def choose_an_action(state, type_function, epsilon, q_size):
     next_state = 0
     if type_function == 1:
-        next_state = greedy(state)
+        next_state = greedy(state, epsilon, q_size)
     else:
         if type_function == 2:
-            next_state = randomAction()
+            next_state = randomAction(state)
     return next_state
+
